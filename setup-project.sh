e4019e5812df79fa6f6a26972c9a016db89c3363
Nicholas Dietz <nicholas.dietz@hotmail.com>
2022-10-15 22:29:24 +0200

    adjust script

--- setup-project.sh
+++ setup-project.sh
@@ -23,19 +23,17 @@ while getopts ":d:g:a:" opt; do
 done
 
 # REPLACE DIRECTORIES
-find . -type d -execdir rename "s/REPLACE_DOMAIN/$DOMAIN_NAME/" '{}' \+
-find . -type d -execdir rename "s/REPLACE_GROUP/$GROUP_NAME/" '{}' \+
-find . -type d -execdir rename "s/REPLACE_APP_NAME/$APP_NAME/" '{}' \+
-
-# REPLACE APP CLASS
-mv ./src/main/kotlin/"$DOMAIN_NAME"/"$GROUP_NAME"/"$APP_NAME"/REPLACE_APP_NAMEApplication.kt ./src/main/kotlin/"$DOMAIN_NAME"/"$GROUP_NAME"/"$APP_NAME"/"$APP_NAME"Application.kt
+mv ./src/main/kotlin/REPLACE_DOMAIN "./src/main/kotlin/$DOMAIN_NAME"
+mv "./src/main/kotlin/$DOMAIN_NAME/REPLACE_GROUP" "./src/main/kotlin/$DOMAIN_NAME/$GROUP_NAME"
+mv "./src/main/kotlin/$DOMAIN_NAME/$GROUP_NAME/REPLACE_APP_NAME" "./src/main/kotlin/$DOMAIN_NAME/$GROUP_NAME/$APP_NAME"
+mv "./src/main/kotlin/$DOMAIN_NAME/$GROUP_NAME/$APP_NAME/REPLACE_APP_NAMEApplication.kt" "./src/main/kotlin/$DOMAIN_NAME/$GROUP_NAME/$APP_NAME/${APP_NAME}Application.kt"
 
 # REPLACE VARS IN ALL FILES
-grep -rl oldtext . --exclude-dir=.git | xargs sed -i "s/REPLACE_DOMAIN/$DOMAIN_NAME/g"
-grep -rl oldtext . --exclude-dir=.git | xargs sed -i "s/REPLACE_GROUP/$GROUP_NAME/g"
-grep -rl oldtext . --exclude-dir=.git | xargs sed -i "s/REPLACE_APP_NAME/$APP_NAME/g"
-grep -rl oldtext . --exclude-dir=.git | xargs sed -i "s/REPLACE_KEYCLOAK_HOST/$KEYCLOAK_HOST/g"
-grep -rl oldtext . --exclude-dir=.git | xargs sed -i "s/REPLACE_KEYCLOAK_PORT/$KEYCLOAK_PORT/g"
+grep -rl REPLACE_DOMAIN . --exclude-dir=.git | xargs sed -i "s/REPLACE_DOMAIN/$DOMAIN_NAME/g"
+grep -rl REPLACE_GROUP . --exclude-dir=.git | xargs sed -i "s/REPLACE_GROUP/$GROUP_NAME/g"
+grep -rl REPLACE_APP_NAME . --exclude-dir=.git | xargs sed -i "s/REPLACE_APP_NAME/$APP_NAME/g"
+grep -rl REPLACE_KEYCLOAK_HOST . --exclude-dir=.git | xargs sed -i "s/REPLACE_KEYCLOAK_HOST/$KEYCLOAK_HOST/g"
+grep -rl REPLACE_KEYCLOAK_PORT . --exclude-dir=.git | xargs sed -i "s/REPLACE_KEYCLOAK_PORT/$KEYCLOAK_PORT/g"
 
 # RUN DOCKER CONTAINERS
 docker run --detach --name "$GROUP_NAME-mariadb" --env MARIADB_USER="$GROUP_NAME" --env MARIADB_PASSWORD="$GROUP_NAME" --env MARIADB_ROOT_PASSWORD="$GROUP_NAME" -p "3306:3306" --env MARIADB_DATABASE="$GROUP_NAME"  mariadb:latest
